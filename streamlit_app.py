46a17e7df7c106f0bc2ecbbd28a250c7e4f2796b
A Lee <40435212+al3393@users.noreply.github.com>
2024-10-28 01:36:39 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,8 @@ else:
             {"role": "assistant", "content": "I am Ayeong's Copilot, your personal assistant for tax inquiries. You can ask me about Title 26 - Internal Revenue Code ."}
         ]
 
-    del st.session_state['chat_copilot']
+    if 'chat_copilot’ in st.session_state.keys():
+	    del st.session_state['chat_copilot’]
     @st.cache_resource
     def load_copilot():
         return Copilot()
